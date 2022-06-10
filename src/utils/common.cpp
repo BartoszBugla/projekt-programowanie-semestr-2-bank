13efc9d24ecc8eee311e870e9f7b8e6368a83af3
mbober01 <michal.bober01@gmail.com>
2022-06-10 23:04:37 +0200

    fix Clear()

--- src/utils/common.cpp
+++ src/utils/common.cpp
@@ -18,8 +18,15 @@ void wait(int milliseconds) {
 }
 
 void Clear() {
+    #if defined _WIN32
     system("cls");
+    #elif defined (__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
+    system("clear");
+    #elif defined (__APPLE__)
+    system("clear");
+    #endif
 }
+
 
 string input() {
     string userInput;
