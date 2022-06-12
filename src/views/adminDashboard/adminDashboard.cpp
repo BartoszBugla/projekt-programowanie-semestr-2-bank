d7643c8feb0fd45662f57d9356e0eb6c850f2a80
BartoszBugla <48729791+BartoszBugla@users.noreply.github.com>
2022-06-12 14:56:01 +0200

    fix admin

--- src/views/adminDashboard/adminDashboard.cpp
+++ src/views/adminDashboard/adminDashboard.cpp
@@ -3,38 +3,88 @@
 //
 #include "adminDashboard.h"
 
+
 void adminDashboard::userData() {
-    int num;
-    cout << "Lista uzytkownikow banku: " << endl;
-    fstream file;
-    int id;
-    string email, password, name, secondname, balance;
-    file.open("baza_uzytkownikow.txt", ios::in);
-    if (file.is_open()) {
-        while (!file.eof()) {
-            file >> id >> email >> password >> name >> secondname;
-            cout << "id = " << id << ", E-mail = " << email << ", Password = " << password
-                 << ", Name = " << name << ", Second Name = " << secondname << endl;
-        }
-        file.close();
-    } else {
-        cout << "Nie udalo sie wczytac listy uzytkownikow" << endl;
+    User *users = User::getUsers();
+    int i = 0;
+    while (users[i].getid()) {
+        cout << color("id: ", Color::green) << users[i].getid() << endl;
+        cout << color("email: ", Color::green) + users[i].email << endl;
+        cout << color("imie: ", Color::green) + users[i].name << endl;
+        cout << color("nazwisko: ", Color::green) + users[i].secondName << endl;
+
+        for (int j = 0; j < 40; j++)
+            cout << color("*", Color::magenta);
+        cout << endl;
+        i++;
     }
-    cout << "1 aby wyjsc do poprzedniego ekranu" << endl;
-    do {
-        num = inputNum();
-    } while (num != 1);
-
-
 }
 
+//    int num;
+//    cout << "Lista uzytkownikow banku: " << endl;
+//    fstream file;
+//    int id;
+//    string email, password, name, secondname, balance;
+//    file.open("baza_uzytkownikow.txt", ios::in);
+//    if (file.is_open()) {
+//        while (!file.eof()) {
+//            file >> id >> email >> password >> name >> secondname;
+//            cout << "id = " << id << ", E-mail = " << email << ", Password = " << password
+//                 << ", Name = " << name << ", Second Name = " << secondname << endl;
+//        }
+//        file.close();
+//    } else {
+//        cout << "Nie udalo sie wczytac listy uzytkownikow" << endl;
+//    }
+//    cout << "1 aby wyjsc do poprzedniego ekranu" << endl;
+//    do {
+//        num = inputNum();
+//    } while (num != 1);
+
+//void adminDashboard::userData()
+//{
+//    int num;
+//    cout<<"Lista uzytkownikow banku: "<<endl;
+//    fstream file;
+//    int id;
+//    string email,password,name,secondname,balance;
+//    file.open("baza_uzytkownikow.txt",ios::in);
+//    if(file.is_open())
+//    {
+//        while(!file.eof())
+//        {
+//            file >> id >> email >> password >> name >> secondname >> balance;
+//            cout<<"id = "<<id<<", E-mail = "<<email<<", Password = "<<password
+//            <<", Name = "<<name<<", Second Name = "<<secondname<<"stan konta: "
+//            <<balance<<endl;
+//        }
+//        file.close();
+//    }
+//    else{
+//        cout<<"Nie udalo sie wczytac listy uzytkownikow"<<endl;
+//    }
+//    cout<<"1 aby wyjsc do poprzedniego ekranu"<<endl;
+//    do
+//    {
+//        num=inputNum();
+//    }while(num!=1);
+//
+//
+//
+//}
+
 void adminDashboard::transferData() {
+    cout << color("Lista przelewow\n", Color::magenta);
+    for (int i = 0; i < 40; i++)
+        cout << color("*", Color::magenta);
+    cout << endl;
     fstream file;
     int id;
     string line;
+
     float value, balanceBefore, balanceAfter;
     string from, to, msg, date;
-    
+
     file.open("Transfers.txt", ios::in);
     cout << color("Lista przelewow\n", Color::magenta);
     for (int i = 0; i < 40; i++)
@@ -44,41 +94,47 @@ void adminDashboard::transferData() {
 
     if (file.is_open()) {
         while (getline(file, line)) {
-            //bartek prosze spojrz o co chodzi
-            string *arrayOfStrings = splitString(line, ";", 8);
-            id = stoi(arrayOfStrings[0]);
-            from = arrayOfStrings[1];
-            to = arrayOfStrings[2];
-            msg = arrayOfStrings[3];
-            value = stof(arrayOfStrings[4]);
-            balanceBefore = stof(arrayOfStrings[5]);
-            balanceAfter = stof(arrayOfStrings[6]);
-            date = arrayOfStrings[7];
 
-            cout << color("id: ", Color::green) << id;
-            cout << color(", od: ", Color::green) + from;
-            cout << color(", do: ", Color::green) + to;
-            cout << color(", wiadomosc: ", Color::green) + msg << endl;
-            cout << color("kwota: ", Color::green) << value;
-            cout << color(", saldo przed: ", Color::green) << balanceBefore;
-            cout << color(", saldo po: ", Color::green) << balanceAfter;
-            cout << color(", data: ", Color::green) + date << endl;
-            for (int i = 0; i < 40; i++)
-                cout << color("*", Color::magenta);
-            cout << endl;
+            float value, balanceBefore, balanceAfter;
+            string from, to, msg, date;
+            file.open("Transfers.txt", ios::in);
+            if (file.is_open()) {
+                while (getline(file, line)) {
 
+
+                    string *arrayOfStrings = splitString(line, ";", 8);
+                    id = stoi(arrayOfStrings[0]);
+                    from = arrayOfStrings[1];
+                    to = arrayOfStrings[2];
+                    msg = arrayOfStrings[3];
+                    value = stof(arrayOfStrings[4]);
+                    balanceBefore = stof(arrayOfStrings[5]);
+                    balanceAfter = stof(arrayOfStrings[6]);
+                    date = arrayOfStrings[7];
+                    cout << color("id: ", Color::green) << id;
+                    cout << color(", od: ", Color::green) + from;
+                    cout << color(", do: ", Color::green) + to;
+                    cout << color(", wiadomosc: ", Color::green) + msg << endl;
+                    cout << color("kwota: ", Color::green) << value;
+                    cout << color(", saldo przed: ", Color::green) << balanceBefore;
+                    cout << color(", saldo po: ", Color::green) << balanceAfter;
+                    cout << color(", data: ", Color::green) + date << endl;
+                    for (int i = 0; i < 40; i++)
+                        cout << color("*", Color::magenta);
+                    cout << endl;
+                }
+                file.close();
+            } else {
+                cout << "Nie udalo sie wczytac listy uzytkownikow" << endl;
+            }
+            Clear();
+            int num;
+            cout << "1 aby wyjsc do poprzedniego ekranu" << endl;
+            do {
+                num = inputNum();
+            } while (num != 1);
         }
-        file.close();
-    } else {
-        cout << "Nie udalo sie wczytac listy uzytkownikow" << endl;
     }
-    Clear();
-
-    int num;
-    cout << "1 aby wyjsc do poprzedniego ekranu" << endl;
-    do {
-        num = inputNum();
-    } while (num != 1);
 }
 
 void adminDashboard::render() {
@@ -100,8 +156,4 @@ void adminDashboard::render() {
     } else if (num == 3) {
         setScreen(welcomeScreen);
     }
-}
-
-adminDashboard::adminDashboard() {
-
 }
